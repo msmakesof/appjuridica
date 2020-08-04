5d40d18f8ee2cd2aa92432a0912a2298a12ddd05
Mauricio Sanchez <msmakesof@gmail.com>
2020-08-03 22:11:34 -0500

    up new file editar_terminos

---
+++ pages/forms/editar_terminos.php
@@ -0,0 +1,44 @@
+<?php
+include_once("../tables/header.inc.php");
+require_once ('../../Connections/DataConex.php'); 
+require_once('../../Connections/config2.php');
+?>
+<?php
+if (!function_exists("GetSQLValueString")) {
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  if (PHP_VERSION < 6) {
+    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+  }
+
+  $theValue = function_exists("mysqli_real_escape_string") ? mysqli_real_escape_string($theValue) : mysqli_escape_string($theValue);
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+}
+
+$idtabla = 0;
+if(isset($_POST['pidtabla'])){
+	$idtabla = trim($_POST['pidtabla']);
+}
+
+require_once('../../apis/proceso/terminos.del.php') ;
+?>
